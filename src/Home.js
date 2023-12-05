bbdab1d4b52dbb12749fe35a04d1f79487ce6101
pogchickennoodlesoup <gcrisolo@terpmail.umd.edu>
2023-12-04 21:26:00 -0500

    Unmodularized home

--- src/Home.js
+++ src/Home.js
@@ -2,31 +2,31 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from "./Home.module.css";
+import "./Home.css";
 
 const Home = () => (
   <>
-    <div className={styles.home}>
-      <img className={styles.about_image}  src="./homeImage.png" alt="Homepage" />
+    <div className="home">
+      <img src="./homeImage.png" alt="Homepage" />
       {/* centered trademark at top <div className="trademark">A Bootcamp Group 3 Production</div>*/}
     </div>
 
-    <div className={styles.container}>
-      <div className={styles.names}>
-        <div className={styles.slice}>
-          <Link to="/ankit" className={styles.rotated_link}>Ankit Amin</Link>
+    <div className="container">
+      <div className="names">
+        <div className="slice">
+          <Link to="/ankit" className="rotated_link">Ankit Amin</Link>
         </div>
-        <div className={styles.slice}>
-          <Link to="/christopher" className={styles.rotated_link}>Christopher Giang</Link>
+        <div className="slice">
+          <Link to="/christopher" className="rotated_link">Christopher Giang</Link>
         </div>
-        <div className={styles.slice}>
-          <Link to="/gavin" className={styles.rotated_link}>Gavin Crisologo</Link>
+        <div className="slice">
+          <Link to="/gavin" className="rotated_link">Gavin Crisologo</Link>
         </div>
-        <div className={styles.slice}>
-          <Link to="/helen" className={styles.rotated_link}>Helen Li</Link>
+        <div className="slice">
+          <Link to="/helen" className="rotated_link">Helen Li</Link>
         </div>
-        <div className={styles.slice}>
-          <Link to="/will-josh" className={styles.rotated_link}>Will-Josh Njeumi</Link>
+        <div className="slice">
+          <Link to="/will-josh" className="rotated_link">Will-Josh Njeumi</Link>
         </div>
       </div> 
     </div>
