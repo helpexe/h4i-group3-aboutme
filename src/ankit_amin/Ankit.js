d8b515c2ee59876b90419c141826f4b44ff34f40
ctgchris <ctgchris@github.com>
2023-12-05 04:36:55 -0500

    fix home page

--- src/ankit_amin/Ankit.js
+++ src/ankit_amin/Ankit.js
@@ -1,9 +1,30 @@
 import React from 'react'
-import "./Ankit.css";
+import styles from "./Ankit.module.css";
 
 function Ankit() {
     return (
-        "hi"
+<body class={styles.container}>
+    <h1 class={styles.font1}>Welcome All!</h1>
+    <h2 class={styles.font2}>My name is Ankit Amin and I am Computer Science a freshman from Dubai (originally from India)</h2>
+    <h2 class={styles.font3}>Favourite Movies: </h2>
+    <h3 class={styles.font2}>
+        <ul class={styles.ul}>
+            <li>Inception</li>
+            <div>
+                <img src="./ankit_amin/hp.jpeg" alt="Harry Potter Movie Poster" />
+            </div>
+            <li>Entire Harry Potter Series</li> 
+        </ul>
+    </h3>
+    <h2 class="font3">Hobbies:</h2>
+    <h3 class="font2">
+        <ul class={styles.ul}>
+            <li>Coding</li>
+            <li>Hitting the gym</li>
+            <li>Listening to music</li>
+        </ul>
+    </h3>
+    </body>
     );
 }
 
