37791feb0b68c5ecc4bb844eae05c291387a5b82
pogchickennoodlesoup <gcrisolo@terpmail.umd.edu>
2023-12-07 16:42:56 -0500

    Added image scaling, framework for links to individual pages

--- src/Home.js
+++ src/Home.js
@@ -7,26 +7,26 @@ import "./Home.css";
 const Home = () => (
   <>
     <div className="home">
-      <img src="./homeImage.png" alt="Homepage" />
+      <img src="./homeImage.png" className="home_image" alt="Homepage" />
       {/* centered trademark at top <div className="trademark">A Bootcamp Group 3 Production</div>*/}
     </div>
 
     <div className="container">
       <div className="names">
         <div className="slice">
-          <Link to="/ankit" className="rotated_link">Ankit Amin</Link>
+          <Link to="/ankit">Ankit Amin</Link>
         </div>
         <div className="slice">
-          <Link to="/christopher" className="rotated_link">Christopher Giang</Link>
+          <Link to="/christopher">Christopher Giang</Link>
         </div>
         <div className="slice">
-          <Link to="/gavin" className="rotated_link">Gavin Crisologo</Link>
+          <Link to="/gavin">Gavin Crisologo</Link>
         </div>
         <div className="slice">
-          <Link to="/helen" className="rotated_link">Helen Li</Link>
+          <Link to="/helen">Helen Li</Link>
         </div>
         <div className="slice">
-          <Link to="/will-josh" className="rotated_link">Will-Josh Njeumi</Link>
+          <Link to="/will-josh">Will-Josh Njeumi</Link>
         </div>
       </div> 
     </div>
